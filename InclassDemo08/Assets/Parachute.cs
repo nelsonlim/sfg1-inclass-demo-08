d54e8a769e3f8173cbf2f48b10508a7559648872
Nelson Lim <me@nelsonlim.com>
2020-03-12 17:48:41 -0500

    Implemented code to deploy a parachute.
    This sets drag, when parachute is within specified distance from the ground.
    
    Showed how to use raycast for this solution.
    
    Tyty mentioned that I should probably opimise this to use a coroutine at some point.

--- InclassDemo08/Assets/Parachute.cs
+++ InclassDemo08/Assets/Parachute.cs
@@ -1,18 +1,46 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Parachute : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float deployHeight = 2.5f;
+    [SerializeField] float drag = 10f;
+
+    Rigidbody rb = null;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        deployParachute();
+    }
+
+    private void deployParachute()
+    {
+        if(isDeployableHeight())
+        {
+            rb.drag = drag;
+        }
+    }
+
+    private bool isDeployableHeight()
+    {
+        Vector3 startPos = transform.position;
+        Vector3 direction = Vector3.down;
+        float maxDist = deployHeight;
+
+        Debug.DrawRay(startPos, direction*maxDist, Color.green);
+        if (Physics.Raycast(startPos, direction, maxDist))
+        {
+            return true;
+        }
+        return false;
     }
 }
